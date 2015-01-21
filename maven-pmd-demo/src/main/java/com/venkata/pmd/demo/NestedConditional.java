93b06c7e64247a5c4ebcd13124cc735d3e58120d
Venkata Saranu <venkata.saranu@gmail.com>
2015-01-21 15:47:01 -0600

    Added Code

---
+++ maven-pmd-demo/src/main/java/com/venkata/pmd/demo/NestedConditional.java
@@ -0,0 +1,13 @@
+package com.venkata.pmd.demo;
+
+public class NestedConditional {
+public void showDetails(){
+	String name=null;
+	String value=null;
+	if(name==null){
+		if(value==null){
+			System.out.println("test");
+		}
+	}
+}
+}
